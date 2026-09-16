8b172327db25bbf9764fea6180c3ec2b9d0ab199
Dike Mahoko <dike@uzinduziafrica.com>
2026-09-16 17:19:29 +0200

    cdn

--- models/constants/media.js
+++ models/constants/media.js
@@ -1,26 +1,30 @@
+const CDN_BASE =
+  process.env.CDN_BASE ||
+  "https://pub-969b935d3cad4df4a4e9c86a6c18588c.r2.dev";
+
 const DEFAULT_PROFILE_PIC =
   process.env.DEFAULT_PROFILE_PIC_URL ||
-  "https://app.uzinduziafrica.com/placeholders/avatar-default.png";
+  `${CDN_BASE}/placeholders/avatar-default.png`;
 
 const DEFAULT_COVER_PHOTO =
   process.env.DEFAULT_COVER_PHOTO_URL ||
-  "https://app.uzinduziafrica.com/placeholders/cover-default.jpg";
+  `${CDN_BASE}/placeholders/cover-default.png`;
 
 const DEFAULT_CORPORATE_LOGO =
   process.env.DEFAULT_CORPORATE_LOGO_URL ||
-  "https://app.uzinduziafrica.com/placeholders/logo-default.png";
+  `${CDN_BASE}/placeholders/logo-default.png`;
 
 const DEFAULT_ALBUM_COVER =
   process.env.DEFAULT_ALBUM_COVER_URL ||
-  "https://app.uzinduziafrica.com/placeholders/album-cover-default.jpg";
+  `${CDN_BASE}/placeholders/album-cover-default.png`;
 
 const DEFAULT_TRACK_ART =
   process.env.DEFAULT_TRACK_ART_URL ||
-  "https://app.uzinduziafrica.com/placeholders/track-art-default.jpg";
+  `${CDN_BASE}/placeholders/track-art-default.png`;
 
 const DEFAULT_PLAQUE_IMAGE =
   process.env.DEFAULT_PLAQUE_IMAGE_URL ||
-  "https://app.uzinduziafrica.com/placeholders/plaque-default.png";
+  `${CDN_BASE}/placeholders/plaque-default.png`;
 
 module.exports = {
   DEFAULT_PROFILE_PIC,
